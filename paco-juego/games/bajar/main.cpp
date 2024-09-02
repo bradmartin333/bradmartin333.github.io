725744af373b539e612eacf3a310cf3134463da3
brad laptop <bradmartin333@gmail.com>
2024-09-01 21:54:07 -0600

    digging guy

--- paco-juego/games/bajar/main.cpp
+++ paco-juego/games/bajar/main.cpp
@@ -4,36 +4,49 @@
 #define HEIGHT 240
 
 enum SolidIcon {
-  ICON_WATER_BOTTLE, // solid
-  ICON_MOUNTAIN, // solid
-  ICON_MAX
+  ICON_WATER_BOTTLE,
+  ICON_MOUNTAIN,
+  ICON_DIGGING,
+  ICON_MAX,
 };
 
 int codepoints[ICON_MAX] = {
-  0xe4c5, // solid
-  0xf6fc, // solid
+  0xe4c5,
+  0xf6fc,
+  0xf85e,
 };
 
-float x = 0.0f;
-float y = 0.0f;
+float x = (float)((WIDTH-40)/2);
+float y = (float)((HEIGHT-36)/2);
+float speed = 1.0;
 
 int main(int argc, char *argv[])
 {
     InitWindow(WIDTH, HEIGHT, "game");
     SetTargetFPS(60);
-    Font fa_regular = LoadFontEx("../include/fa-regular.ttf", 36, codepoints, ICON_MAX);
     Font fa_solid = LoadFontEx("../include/fa-solid.ttf", 36, codepoints, ICON_MAX);
     while (!WindowShouldClose())
     {
         BeginDrawing();
-        ClearBackground(RAYWHITE);
-        DrawTextCodepoint(fa_solid, codepoints[ICON_WATER_BOTTLE], (Vector2){x, y}, 36, MAROON);
-        x += 1.0f;
-        y += 3.0f;
-        if (y + 36 > HEIGHT)
-          y = 0;
-        if (x + 36 > WIDTH)
+        ClearBackground(BLACK);
+        
+        DrawTextCodepoint(fa_solid, codepoints[ICON_DIGGING], (Vector2){x, y}, 36, YELLOW);
+        
+        if (IsKeyDown(KEY_LEFT)) x -= 1 * speed;
+        if (IsKeyDown(KEY_RIGHT)) x += 1 * speed;
+        if (IsKeyDown(KEY_DOWN)) y += 1 * speed;
+        if (IsKeyDown(KEY_UP)) y -= 1 * speed;
+        if (IsKeyPressed(KEY_SPACE)) speed *= 2;
+
+        if (x < 0)
           x = 0;
+        if (x > WIDTH-40)
+          x = WIDTH-40;
+        if (y < 0)
+          y = 0;
+        if (y > HEIGHT-36)
+          y = HEIGHT-36;
+        
         EndDrawing();
     }
     CloseWindow();
